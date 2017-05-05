b635fd3a89eb64042b48a2e33eccc9a5b26df5bc
cxz <1604101058@qq.com>
2017-05-05 22:08:37 +0800

    添加了NJDog类

---
+++ May_five_weChatTest/May_five_weChatTest/NJDog.h
@@ -0,0 +1,13 @@
+//
+//  NJDog.h
+//  May_five_weChatTest
+//
+//  Created by TouchWorld on 2017/5/5.
+//  Copyright © 2017年 cxz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NJDog : NSObject
+
+@end
---
+++ May_five_weChatTest/May_five_weChatTest/NJDog.m
@@ -0,0 +1,13 @@
+//
+//  NJDog.m
+//  May_five_weChatTest
+//
+//  Created by TouchWorld on 2017/5/5.
+//  Copyright © 2017年 cxz. All rights reserved.
+//
+
+#import "NJDog.h"
+
+@implementation NJDog
+
+@end
